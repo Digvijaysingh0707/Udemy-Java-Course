e81395d4ace0ae0ae71112238bf6493c2a72b277
Digvijaysingh0707 <47813702+Digvijaysingh0707@users.noreply.github.com>
2019-12-28 01:19:08 +0530

    Create Main.java

---
+++ Section 7: OOP Part 2 - Composition, Encapsulation, and Polymorphism/Encapsulation/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("Digvijay",200,"Knife");
+        System.out.print("Initial health is:- "+player.getHitpoints());
+    }
+
+       }
